eebf28edd8e66ef940ac2e57f5f1ca6ec55884f3
voroshen <voroshen.av@gmail.com>
2019-03-12 00:07:40 +0300

    master: initial commit

---
+++ src/main/java/com/voroshen/mapspotinfohunterapi/MapSpotInfoHunterApiApplication.java
@@ -0,0 +1,13 @@
+package com.voroshen.mapspotinfohunterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapSpotInfoHunterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapSpotInfoHunterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/voroshen/mapspotinfohunterapi/MapSpotInfoHunterApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.voroshen.mapspotinfohunterapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MapSpotInfoHunterApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
