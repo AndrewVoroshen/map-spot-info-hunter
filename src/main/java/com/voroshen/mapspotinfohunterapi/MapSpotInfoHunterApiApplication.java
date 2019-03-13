7e4df70ee68ceee08e507bd5dc3f8a2b75a2f6ac
voroshen <voroshen.av@gmail.com>
2019-03-13 11:18:04 +0300

    master: add test values to database

--- src/main/java/com/voroshen/mapspotinfohunterapi/MapSpotInfoHunterApiApplication.java
+++ src/main/java/com/voroshen/mapspotinfohunterapi/MapSpotInfoHunterApiApplication.java
@@ -1,7 +1,20 @@
 package com.voroshen.mapspotinfohunterapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.List;
 
 @SpringBootApplication
 public class MapSpotInfoHunterApiApplication {
@@ -10,4 +23,48 @@ public class MapSpotInfoHunterApiApplication {
 		SpringApplication.run(MapSpotInfoHunterApiApplication.class, args);
 	}
 
+	@Bean
+	ApplicationRunner applicationRunner(CustomRepository repo) {
+		return args -> {
+			repo.save(new CustomEntity());
+			repo.save(new CustomEntity());
+			repo.save(new CustomEntity());
+		};
+	}
+}
+
+@RestController
+class ApiController {
+
+	private final CustomRepository repo;
+
+	@Autowired
+	ApiController(CustomRepository repo) {
+		this.repo = repo;
+	}
+
+	@GetMapping("/")
+	String test() {
+		return "test";
+	}
+
+	@GetMapping("/entities")
+	List<CustomEntity> testEntities() {
+		return repo.getEntities();
+	}
+}
+
+@Entity
+@Table(name = "custom")
+class CustomEntity {
+
+	@Id
+	@GeneratedValue
+	public Long id;
+}
+
+@Repository
+interface CustomRepository extends JpaRepository<CustomEntity, Long> {
+
+	List<CustomEntity> getEntities();
 }
