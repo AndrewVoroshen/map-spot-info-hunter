a739011018b24eb0749895a069e5a0018d0419c8
voroshen <voroshen.av@gmail.com>
2019-04-14 20:13:10 +0300

    master: getCurrentUser id method implemented

--- src/main/java/com/voroshen/mapspotinfohunterapi/security/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/voroshen/mapspotinfohunterapi/security/service/impl/UserDetailsServiceImpl.java
@@ -1,10 +1,14 @@
 package com.voroshen.mapspotinfohunterapi.security.service.impl;
 
 import com.voroshen.mapspotinfohunterapi.security.principle.UserPrinciple;
+import com.voroshen.mapspotinfohunterapi.security.service.SecurityService;
 import com.voroshen.mapspotinfohunterapi.user.entity.UserEntity;
 import com.voroshen.mapspotinfohunterapi.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,7 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
-public class UserDetailsServiceImpl implements UserDetailsService {
+public class UserDetailsServiceImpl implements UserDetailsService, SecurityService {
 
 	private final UserRepository userRepository;
 
@@ -25,5 +29,13 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 				() -> new UsernameNotFoundException("User Not Found with -> username or email : " + username));
 
 		return UserPrinciple.build(userEntity);
+	}
+
+	@Override
+	public Long getCurrentUserId() {
+		SecurityContext secureContext = SecurityContextHolder.getContext();
+		Authentication authentication = secureContext.getAuthentication();
+		UserPrinciple principle = (UserPrinciple) authentication.getPrincipal();
+		return principle.getId();
 	}
 }
